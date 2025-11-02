1e9ba5adea040dc4aa607fd75dc24438fe9951e9
JungYeonWoong <dusdnd0209@naver.com>
2025-11-03 00:24:28 +0900

    feat(input): Add purchase amount input feature

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,11 @@
+package lotto.controller;
+
+import lotto.view.InputView;
+
+public class LottoController {
+
+    public void run() {
+        int purchaseAmount = InputView.inputPurchaseAmount();
+        System.out.println(purchaseAmount);
+    }
+}
