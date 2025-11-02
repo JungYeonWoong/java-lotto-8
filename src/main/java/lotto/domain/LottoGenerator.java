b8bf4b461d4ff2e8e71ebd7a7a2a75950159e6ea
JungYeonWoong <dusdnd0209@naver.com>
2025-11-03 01:01:34 +0900

    feat(lotto): Sort generated lotto numbers in ascending order

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -3,17 +3,17 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.Lotto;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoGenerator {
 
-    // 로또 한 장(6개 번호) 생성
     public static Lotto createLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
         return new Lotto(numbers);
     }
 
-    // 구입 개수만큼 로또 생성
     public static List<Lotto> createLottos(int lottoCount) {
         List<Lotto> lottos = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
