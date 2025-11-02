354e761a3db1a742c84b6d7dac23033b99067eb3
JungYeonWoong <dusdnd0209@naver.com>
2025-11-03 00:27:14 +0900

    feat(validation): Throw error when purchase amount is not a number

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,6 +7,11 @@ public class InputView {
     public static int inputPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
-        return Integer.parseInt(input.trim());
+
+        try {
+            return Integer.parseInt(input.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
     }
 }
