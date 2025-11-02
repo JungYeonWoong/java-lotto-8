fdff9075e3c91c7abac11739b8e26ae045b68391
JungYeonWoong <dusdnd0209@naver.com>
2025-11-03 00:37:41 +0900

    feat(calculation): Calculate lotto count from purchase money

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -5,7 +5,12 @@ import lotto.view.InputView;
 public class LottoController {
 
     public void run() {
-        int purchaseAmount = InputView.inputPurchaseAmount();
-        System.out.println(purchaseAmount);
+        int purchaseMoney = InputView.inputPurchaseAmount();
+        int lottoCount = calculateLottoCount(purchaseMoney);
+        System.out.println(lottoCount + "개를 구매했습니다.");
+    }
+
+    private int calculateLottoCount(int purchaseMoney) {
+        return purchaseMoney / 1000;
     }
 }
