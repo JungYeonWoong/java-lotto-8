70bae5367248b2cdf9563fefa80b906041b344d7
JungYeonWoong <dusdnd0209@naver.com>
2025-11-03 17:20:02 +0900

    feat(view): Display winning statistics in correct format using Rank enum

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,9 @@
 package lotto.view;
 
 import lotto.Lotto;
+import lotto.domain.Rank;
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
@@ -11,7 +13,17 @@ public class OutputView {
 
     public static void printGeneratedLottos(List<Lotto> lottos) {
         for (Lotto lotto : lottos) {
-            System.out.println(lotto); // ✅ lotto.toString() 자동 호출
+            System.out.println(lotto); // lotto.toString() 자동 호출
         }
+    }
+
+    // 당첨 통계 출력
+    public static void printWinningStatistics(Map<Rank, Integer> results) {
+
+        System.out.printf("%s - %d개%n", Rank.FIFTH.getDescription(), results.getOrDefault(Rank.FIFTH, 0));
+        System.out.printf("%s - %d개%n", Rank.FOURTH.getDescription(), results.getOrDefault(Rank.FOURTH, 0));
+        System.out.printf("%s - %d개%n", Rank.THIRD.getDescription(), results.getOrDefault(Rank.THIRD, 0));
+        System.out.printf("%s - %d개%n", Rank.SECOND.getDescription(), results.getOrDefault(Rank.SECOND, 0));
+        System.out.printf("%s - %d개%n", Rank.FIRST.getDescription(), results.getOrDefault(Rank.FIRST, 0));
     }
 }
